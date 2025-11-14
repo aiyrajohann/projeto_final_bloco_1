070a9c7b7e75c07e9d342fdc82fa508fe96aadbc
Aiyra Johann <aiyra.j.araujo22@gmail.com>
2025-11-14 11:29:19 -0300

    feat: Implementa menu principal e cores

---
+++ Menu.ts
@@ -0,0 +1,106 @@
+import readlinesync = require("readline-sync");
+import { colors } from "./src/util/Colors";
+
+export function main(): void {
+  let opcao: number;
+
+  while (true) {
+    console.log(colors.bg.red, colors.fg.whitestrong);
+    console.log("*****************************************************");
+    console.log("                                                     ");
+    console.log("          IMOBILIÁRIA - PROJETO FINAL                ");
+    console.log("                                                     ");
+    console.log("*****************************************************");
+    console.log("                                                     ");
+    console.log("         1 - Cadastrar corretor                      ");
+    console.log("         2 - Listar corretores                       ");
+    console.log("         3 - Cadastrar imóvel                        ");
+    console.log("         4 - Listar imóveis                          ");
+    console.log("         5 - Atribuir corretor a imóvel              ");
+    console.log("         6 - Vender imóvel                           ");
+    console.log("         7 - Relatório de comissões                  ");
+    console.log("         0 - Sair                                    ");
+    console.log("                                                     ");
+    console.log("*****************************************************");
+    console.log("                                                     ");
+    console.log(colors.reset);
+
+    console.log("Entre com a opção desejada: ");
+    opcao = readlinesync.questionInt("");
+
+    if (opcao === 0) {
+      console.log(colors.fg.greenstrong, "\nImobiliária - Obrigado e até logo!");
+      sobre();
+      console.log(colors.reset, "");
+      process.exit(0);
+    }
+
+    switch (opcao) {
+      case 1:
+        console.log(colors.fg.whitestrong, "\n\nCadastrar corretor\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Corretores.", colors.reset);
+        keyPress();
+        break;
+
+      case 2:
+        console.log(colors.fg.whitestrong, "\n\nListar corretores\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Corretores.", colors.reset);
+        keyPress();
+        break;
+
+      case 3:
+        console.log(colors.fg.whitestrong, "\n\nCadastrar imóvel\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Imóveis.", colors.reset);
+        keyPress();
+        break;
+
+      case 4:
+        console.log(colors.fg.whitestrong, "\n\nListar imóveis\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Imóveis.", colors.reset);
+        keyPress();
+        break;
+
+      case 5:
+        console.log(colors.fg.whitestrong, "\n\nAtribuir corretor a imóvel\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Atribuição.", colors.reset);
+        keyPress();
+        break;
+
+      case 6:
+        console.log(colors.fg.whitestrong, "\n\nVender imóvel\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Vendas.", colors.reset);
+        keyPress();
+        break;
+
+      case 7:
+        console.log(colors.fg.whitestrong, "\n\nRelatório de comissões\n\n", colors.reset);
+        console.log(colors.fg.yellow, "Funcionalidade será implementada na branch de Relatórios.", colors.reset);
+        keyPress();
+        break;
+
+      default:
+        console.log(colors.fg.red, "\nOpção Inválida!", colors.reset);
+        keyPress();
+        break;
+    }
+  }
+}
+
+/* Função sobre */
+function sobre(): void {
+  console.log("\n*****************************************************");
+  console.log("Projeto Desenvolvido por: Aiyra Johann");
+  console.log("Email: aiyra.j.araujo@hotmail.com");
+  console.log("github.com/aiyrajohann");
+  console.log("*****************************************************");
+}
+
+
+function keyPress(): void {
+  console.log(colors.reset, "");
+  console.log("\nPressione enter para continuar...");
+  readlinesync.prompt();
+}
+
+main();
+
---
+++ src/util/Colors.ts
@@ -0,0 +1,45 @@
+// src/util/Colors.ts
+
+export const colors = {
+    reset: "\x1b[0m",
+
+    fg: {
+        black: "\x1b[30m",
+        red: "\x1b[31m",
+        green: "\x1b[32m",
+        yellow: "\x1b[33m",
+        blue: "\x1b[34m",
+        magenta: "\x1b[35m",
+        cyan: "\x1b[36m",
+        white: "\x1b[37m",
+
+        blackstrong: "\x1b[30;1m",
+        redstrong: "\x1b[31;1m",
+        greenstrong: "\x1b[32;1m",
+        yellowstrong: "\x1b[33;1m",
+        bluestrong: "\x1b[34;1m",
+        magentastrong: "\x1b[35;1m",
+        cyanstrong: "\x1b[36;1m",
+        whitestrong: "\x1b[37;1m",
+    },
+
+    bg: {
+        black: "\x1b[40m",
+        red: "\x1b[41m",
+        green: "\x1b[42m",
+        yellow: "\x1b[43m",
+        blue: "\x1b[44m",
+        magenta: "\x1b[45m",
+        cyan: "\x1b[46m",
+        white: "\x1b[47m",
+
+        blackstrong: "\x1b[40;1m",
+        redstrong: "\x1b[41;1m",
+        greenstrong: "\x1b[42;1m",
+        yellowstrong: "\x1b[43;1m",
+        bluestrong: "\x1b[44;1m",
+        magentastrong: "\x1b[45;1m",
+        cyanstrong: "\x1b[46;1m",
+        whitestrong: "\x1b[47;1m",
+    }
+};
